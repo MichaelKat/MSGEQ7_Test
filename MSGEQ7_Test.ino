3449e41bad5ae7d507317a39533ebddffe13cf35
Michael Katilevsky <katilevsky.michael@gmail.com>
2018-08-29 22:36:39 -0400

    Added an elaborate description of MSGEQ7_Test.ino; Eliminated some code duplication; Changed some global variables to local variables to save memory space.

--- MSGEQ7_Test.ino
+++ MSGEQ7_Test.ino
@@ -1,53 +1,52 @@
-
 // **********************************************************
-//   MSGEQ7_Test.ino
+//  MSGEQ7_Test.ino
 //
-//   
-//   
-//   Author: Michael Katilevsky
+//  The MSGEQ7 chip analyzes an audio signal and outputs the 
+//  intensity of seven frequency ranges as analog values. 
+//  However, the MSGEQ7 can only output one value at a time, 
+//  so the Arduino must "strobe" the MSGEQ7 to read the 
+//  intensities of all the seven frequency ranges.
 //
-//   26 December 2015
+//  The outputs are on an "on/off" basis: "on" when the
+//  frequency range is loud enough to meet the "sensibility"
+//  treshold, "off" when not loud enough. 
+//
+//  An easy improvement could be to output each frequency's 
+//  intensity on a PWM basis, where the LED's turn brighter 
+//  as the corresponding frequency range gets louder.
+//
+//  Connect the output of the MSGEQ7 to Arduino's analog pin
+//  5. Wire up the MSGEQ7 as per its datasheet.
+//   
+//  Author: Michael Katilevsky
+//
+//  26 December 2015
 //
 // **********************************************************
 
-const byte Strobe = 12;       //digital pins  
-const byte Reset = 13;
+const byte strobe = 12;       //digital pins  
+const byte reset = 13;
 
-int Freq[7];        // MSGEQ7 value 
+const byte tStrobe = 20;   // Strobe duration in microseconds
+const byte tSettle = 40;   // Output settling time in microseconds
 
-const byte TStrobe = 20;   // Strobe duration in microseconds
-const byte TSettle = 40;   // Output settling time in microseconds
-
-byte b;
-
-int Sensability = 300;
+// How powerful/loud the frequency range must be to trigger the output
+int sensibility = 300; // value must be between 0 and 1023
 
 void setup() 
 {
   
-  pinMode(Strobe, OUTPUT);
-  pinMode(Reset, OUTPUT);
+  pinMode(strobe, OUTPUT);
+  pinMode(reset, OUTPUT);
 
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(7, OUTPUT);
-  pinMode(8, OUTPUT);
-  pinMode(9, OUTPUT);
+  for(byte a = 3; a < 10; a++) {
+    pinMode(a, OUTPUT);
+    digitalWrite(a, LOW);
+  }
 
-  digitalWrite(3, LOW);
-  digitalWrite(4, LOW);
-  digitalWrite(5, LOW);
-  digitalWrite(6, LOW);
-  digitalWrite(7, LOW);
-  digitalWrite(8, LOW);
-  digitalWrite(9, LOW);
-  
-
-
-  digitalWrite(Reset, LOW);
-  digitalWrite(Strobe, HIGH);
+  // Initialize the chip
+  digitalWrite(reset, LOW);
+  digitalWrite(strobe, HIGH);
   
   delay(100);
 
@@ -61,24 +60,27 @@ void setup()
 
 void loop()
 {
-    
-  digitalWrite(Reset, HIGH);
-  digitalWrite(Reset, LOW);      
+  // Declare an array to store the intensities of the seven frequency ranges
+  int freq[7];
   
-  for(b = 0; b < 7; b++)    // cycle through frequencies
+  digitalWrite(reset, HIGH);
+  digitalWrite(reset, LOW);      
+  
+  for(byte b = 0; b < 7; b++)    // cycle through frequencies
   {
-    digitalWrite(Strobe, LOW);
-    delayMicroseconds(TSettle);
+    digitalWrite(strobe, LOW);
+    delayMicroseconds(tSettle);
 
-    Freq[b] = analogRead(5);
+    freq[b] = analogRead(5); // store the value
 
-    digitalWrite(Strobe, HIGH);
-    delayMicroseconds(TStrobe);
+    digitalWrite(strobe, HIGH);
+    delayMicroseconds(tStrobe);
   }
 
-  for(b = 3; b < 10; b++)
+  // Output the frequencies' intensity
+  for(byte b = 3; b < 10; b++)
   {
-    if(Freq[b-3] > Sensability)
+    if(freq[b-3] > sensibility)
     {
       digitalWrite(b, HIGH);
       Serial.print(" ");
